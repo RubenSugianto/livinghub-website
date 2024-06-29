3c1b6d0d104f147db0f10693bbba8de5848ec58d
RubenSugianto <90769407+RubenSugianto@users.noreply.github.com>
2024-06-30 01:04:17 +0700

    Revert "Dev ruben"

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,15 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-
-    <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
-    
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap');
 
@@ -256,7 +247,6 @@
             Living HUB
         </div>
         <nav>
-            <a href="{{ route('register') }}">Register</a>
             <a href="{{ route('home') }}">Home</a>
             <a href="{{ route('dijual') }}">Dijual</a>
             <a href="{{ route('disewa') }}">Disewa</a>
@@ -329,6 +319,5 @@
             <p>&copy; 2024 Living HUB. All rights reserved.</p>
         </div>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,3 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 Route::get('/dijual', [App\Http\Controllers\DijualController::class, 'index'])->name('dijual');
 Route::get('/disewa', [App\Http\Controllers\DisewaController::class, 'index'])->name('disewa');
 Route::get('/simulasikpr', [App\Http\Controllers\DisewaController::class, 'index'])->name('simulasikpr');
-
-Route::get('/register', [App\Http\Controllers\RegisterController::class, 'index'])->name('register');
-Route::post('/register', [App\Http\Controllers\RegisterController::class, 'store']);
-
-Route::get('/login', [App\Http\Controllers\LoginController::class, 'index'])->name('login');
-Route::post('/login', [App\Http\Controllers\LoginController::class, 'authenticate']);
