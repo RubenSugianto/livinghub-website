619e4a8cfd78436255d1088c93b0d70ec2cc3e6f
Fellyne <feliciaedelyne@gmail.com>
2024-06-30 22:10:04 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DijualController;
+use App\Http\Controllers\DisewaController;
+use App\Http\Controllers\SimulasikprController;
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/dijual', [App\Http\Controllers\DijualController::class, 'index'])->name('dijual');
-Route::get('/disewa', [App\Http\Controllers\DisewaController::class, 'index'])->name('disewa');
-Route::get('/simulasikpr', [App\Http\Controllers\DisewaController::class, 'index'])->name('simulasikpr');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/dijual', [DijualController::class, 'index'])->name('dijual');
+Route::get('/disewa', [DisewaController::class, 'index'])->name('disewa');
+Route::get('/simulasikpr', [SimulasikprController::class, 'index'])->name('simulasikpr');
+Route::post('/simulasikpr/calculate', [SimulasikprController::class, 'calculate'])->name('simulasikpr.calculate');
+
 
 Route::get('/register', [App\Http\Controllers\RegisterController::class, 'index'])->name('register');
 Route::post('/register', [App\Http\Controllers\RegisterController::class, 'store']);
