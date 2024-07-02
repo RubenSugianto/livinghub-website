af7da826fe0b252dec9ebaba6434d19d1f1dbc44
RubenSugianto <90769407+RubenSugianto@users.noreply.github.com>
2024-07-02 18:47:46 +0700

    Add User Model. User Factory, User Table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,19 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'id' => (string) Str::uuid(),
+            'fullname' => $this->faker->unique()->name(),
+            'username' => $this->faker->unique()->userName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'gender' => $this->faker->randomElement(['male', 'female', 'other']),
+            'age' => $this->faker->numberBetween(18, 90),
+            'phone' => $this->faker->unique()->regexify('[0-9]{12}'),
+            'password' => Hash::make('password'),
+            'role' => 'user',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 
