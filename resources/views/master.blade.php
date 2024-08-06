73bbc6f9646269dc97da6e5b72a271f2b0ab658f
Fellyne <feliciaedelyne@gmail.com>
2024-08-06 22:50:41 +0700

    Benerin bootstrap

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
     <!-- Font Awesome -->
@@ -64,14 +65,14 @@
         }
 
         nav a {
-            color: #fff;
-            font-size: 12px;
-            font-weight: 500;
-            text-decoration: none;
-            padding: 5px 5px;
-            position: relative;
-            transition: color 0.3s ease;
-        }
+        color: #fff;
+        font-size: 12px !important;
+        font-weight: 500 !important;
+        text-decoration: none !important;
+        padding: 5px 5px !important;
+        position: relative !important;
+        transition: color 0.3s ease !important;
+         }
 
         nav a:before {
             content: '';
@@ -497,10 +498,11 @@
                 <p>&copy; 2024 Living HUB. All rights reserved.</p>
             </div>
         </footer>
-
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         @yield('scripts')
     </body>
     </html>
