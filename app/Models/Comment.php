951b8e4b59b286f35e99329342fea736f52756fa
nicooo2380 <nicolaliu75@gmail.com>
2024-09-13 22:17:38 +0700

    fitur komentar

---
+++ app/Models/Comment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Comment extends Model
+{
+    // use HasFactory;
+
+    // Mengatur properti yang dapat diisi (mass assignable)
+    protected $fillable = ['property_id', 'user_name', 'comment'];
+
+    // Mengatur tipe data UUID untuk primary key
+    protected $keyType = 'uuid';
+
+    // Mengatur primary key tidak auto-increment
+    public $incrementing = false;
+
+    // Menginisialisasi model dengan UUID saat membuat entri baru
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    // Relasi many-to-one dengan Property
+    public function property()
+    {
+        return $this->belongsTo(Property::class, 'property_id');
+    }
+}
---
+++ database/migrations/2024_09_09_034753_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('property_id')->constrained('properties')->onDelete('cascade'); // Relasi ke properties dengan tipe UUID
+            $table->string('user_name')->nullable()->change();
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+
+};
