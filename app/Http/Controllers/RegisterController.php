d75b082bfeae8cd40f335c4d6b92a123c7685ba7
RubenSugianto <ruben.sugianto@gmail.com>
2024-09-27 23:16:46 +0700

    Change Fullname to name in Register Controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -14,7 +14,7 @@ class RegisterController extends Controller
 
     public function store(Request $request) {
         $validatedData = $request->validate([
-            'fullname' => 'required|max:255|unique:users',
+            'name' => 'required|max:255|unique:users',
             'username' => ['required', 'min:3', 'max:255', 'unique:users'],
             'email' => 'required|email:dns|unique:users',
             'password' => 'required|min:5|max:255',
