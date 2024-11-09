1c2397ad13913923529762297d3c5439e2ecfdcc
RubenSugianto <ruben.sugianto@gmail.com>
2024-11-09 16:42:51 +0700

    Add Indonesian Language

---
+++ lang/id/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan oleh pustaka paginator untuk membangun
+    | tautan paginasi sederhana. Anda bebas untuk mengubahnya sesuai
+    | dengan kebutuhan tampilan aplikasi Anda.
+    |
+    */
+
+    'previous' => '&laquo; Sebelumnya',
+    'next' => 'Berikutnya &raquo;',
+
+];
