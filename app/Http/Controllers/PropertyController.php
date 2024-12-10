2635d2af600cdc69b6cc53402a0b26e891ff5bea
Fellyne <feliciaedelyne@gmail.com>
2024-12-10 21:58:41 +0700

    Benerin css add prop dan prop detail

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -43,7 +43,7 @@ class PropertyController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:20'],
             'price' => ['required', 'numeric', 'min:0'], 
             'province' => ['required', 'string'],
             'location' => ['required', 'string'],
